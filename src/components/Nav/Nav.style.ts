6f0bd3474f089b2e92299a0911cbb98fc91149fa
nrbns357 <gusgh357@gmail.com>
2022-09-07 22:08:59 +0900

    ⚡:: nav style 변경

---
+++ src/components/Nav/Nav.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const NavBarContainer = styled.div`
+  width: 150px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+export const MusicChart = styled.div`
+  padding-top: 130px;
+`;
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import { NavBarContainer, MusicChart } from "./Nav.style";
+
+const Nav = () => {
+  return (
+    <NavBarContainer>
+      <MusicChart>
+        기상송 차트
+      </MusicChart>
+    </NavBarContainer>
+  );
+};
+
+export default Nav;
