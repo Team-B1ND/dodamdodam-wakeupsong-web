c5dc7c4ac9898015d1621ee1eeecf236a4ec9905
nrbns357 <gusgh357@gmail.com>
2022-09-12 20:13:10 +0900

    ⚡:: 기상송 신청 컴포넌트 생성

---
+++ src/components/Main/ApplyMusic/ApplyMusic.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ApplyMusicContainer = styled.div`
+  width: 400px;
+  height: 600px;
+  background-color: skyblue;
+`;
---
+++ src/components/Main/ApplyMusic/ApplyMusic.tsx
@@ -0,0 +1,11 @@
+import * as S from "./ApplyMusic.style";
+
+const ApplyMusic = () => {
+  return (
+    <S.ApplyMusicContainer>
+
+    </S.ApplyMusicContainer>
+  );
+};
+
+export default ApplyMusic;
