eca655d95904ca8c3d83a2e56d426dd41e2de511
nrbns357 <gusgh357@gmail.com>
2022-09-14 10:52:02 +0900

    ⚡:: TitleMent common으로 빼기

---
+++ src/components/Common/Title/Title.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MusicListTitle = styled.div`
+  display: grid;
+  gap: 5px;
+`;
+
+export const TitleMent = styled.div`
+  font-weight: bold;
+  font-size: 1.3rem;
+`;
+
+export const SubTitleMent = styled.div`
+  font-size: 0.9rem;
+`;
