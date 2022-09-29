f6b021af743accd13fc66b526d2019e5b5c85065
nrbns357 <gusgh357@gmail.com>
2022-09-29 20:43:30 +0900

    ⚡:: refresh token 관리 로직 구현

---
+++ src/repository/token/token.param.ts
@@ -0,0 +1,3 @@
+export interface postRefreshTokenParam {
+  token: string;
+}
---
+++ src/types/token/token.type.ts
@@ -0,0 +1,5 @@
+import { Response } from "../util/response.type";
+
+export interface RefreshResponse extends Response {
+  data: string;
+}
