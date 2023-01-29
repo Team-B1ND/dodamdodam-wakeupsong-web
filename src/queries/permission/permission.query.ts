7ce1c75b85b9e4a81ebfcd9aac5b2d0f89f3b336
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-01-30 01:37:58 +0900

    🐛 ::

--- src/queries/permission/permission.query.ts
+++ src/queries/permission/permission.query.ts
@@ -1,7 +1,9 @@
 import { useQuery } from "react-query";
+// import permissionRepository from "repository/permission/permission.repository";
 import permissionRepository from "repository/permission/permission.repository";
 
 export const useGetMyPermission = () =>
   useQuery("permission/getMyPermission", () =>
     permissionRepository.getMyPermissionMy()
   );
+  
