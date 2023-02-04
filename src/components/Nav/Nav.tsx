b11bef4052e0b56adbc08482766e878822dabf2f
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-02-04 18:27:41 +0900

    코드리뷰

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,11 +2,17 @@ import * as NavStyle from "./Nav.style";
 import DODAMLOGO from "assets/logo_img/Mask group.svg";
 import ApplyMusic from "./ApplyMusic";
 import MelonChart from "./MelonChart";
+import ErrorBoundary from "components/Common/Errorboundary/Errordata";
+import { Suspense } from "react";
 
 const Nav = () => {
   return (
     <NavStyle.NavBarContainer>
-      <NavStyle.DodamLogo src={DODAMLOGO} alt="dodam_logo" onClick={() => window.location.href = "http://dodam.b1nd.com"} />
+      <NavStyle.DodamLogo
+        src={DODAMLOGO}
+        alt="dodam_logo"
+        onClick={() => (window.location.href = "http://dodam.b1nd.com")}
+      />
       <ApplyMusic />
       <MelonChart />
     </NavStyle.NavBarContainer>
--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,30 +1,29 @@
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-import Header from "../Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../Header";
 import Main from "../Main";
 import Nav from "../Nav";
 import DibsStorage from "components/DibsStorage";
 import MyMusicStore from "components/MyMusicDetail";
 import TodayMusicDetail from "components/TodayMusicDetail";
 import PendingMusicDetail from "components/PendingMusicDetail";
+import { Suspense, lazy } from "react";
+
+// const MyMusicStore = lazy(() => import("./routes/MyMusicStore"));///
 
 const Router = () => {
-    return (
-        <BrowserRouter basename="/wakesong">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/dibsstorage" element={<DibsStorage />} />
-                <Route path="/mymusicdetail" element={<MyMusicStore />} />
-                <Route path="/todaymusicdetail" element={<TodayMusicDetail />} />
-                <Route path="/pendingmusicdetail" element={<PendingMusicDetail />} />
-            </Routes>
-            <Nav />
-        </BrowserRouter >
-    )
-}
+  return (
+    <BrowserRouter basename="/wakesong">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/dibsstorage" element={<DibsStorage />} />
+        <Route path="/mymusicdetail" element={<MyMusicStore />} />
+        <Route path="/todaymusicdetail" element={<TodayMusicDetail />} />
+        <Route path="/pendingmusicdetail" element={<PendingMusicDetail />} />
+      </Routes>
+      <Nav />
+    </BrowserRouter>
+  );
+};
 
 export default Router;
