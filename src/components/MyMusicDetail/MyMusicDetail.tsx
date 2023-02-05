a4a3a906684ef0ecb8c1a15d430d80ed26046cd8
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-02-05 23:37:38 +0900

    ⚡:: 에러바운더리 적용

--- src/components/MyMusicDetail/MyMusicDetail.tsx
+++ src/components/MyMusicDetail/MyMusicDetail.tsx
@@ -2,15 +2,15 @@ import Title from "components/Common/Title";
 import * as MyMusicDetailStyle from "./MyMusicDetail.style";
 import { BsTrash } from "react-icons/bs";
 import useDeleteMyMusic from "hooks/deleteMyMusic/useDeleteMyWakeupSong";
-import { useGetMyAllWakeupSong } from "queries/myAllWakeupSong/myAllWakeupSong.query";
 import ErrorBoundary from "components/Common/Errorboundary/Errordata";
 import { Suspense } from "react";
+import MyMusicList from "./MyMusicList";
+// import { MyMusiceList } from "./MyMusicList";
 // import { ErrorBoundary } from "react-error-boundary";
 // import { Errordata } from "../Common/Errorboundary/Errordata";
 
 const MyMusicStore = () => {
   const { deleteMyWakeupSong } = useDeleteMyMusic();
-  const myData = useGetMyAllWakeupSong().data?.data;
 
   return (
     <MyMusicDetailStyle.MyMusicDetailWrapContainer>
@@ -18,50 +18,11 @@ const MyMusicStore = () => {
         titleMent={"마이뮤직"}
         subTitleMent={"내가 신청한 기상송을 한눈에 보세요!"}
       />
-
-      <MyMusicDetailStyle.MyMusicListWrap>
-        {myData?.length !== 0 ? (
-          myData?.map((item, idx) => {
-            const createdDate = item.createdDate.split(" ")[0];
-            return (
-              <ErrorBoundary fallback={<div>error ...</div>}>
-                <Suspense fallback={<div>Loading...</div>}>
-                  <MyMusicDetailStyle.MusicInfoContainer>
-                    <MyMusicDetailStyle.ApplyRanking>
-                      {idx + 1}
-                    </MyMusicDetailStyle.ApplyRanking>
-                    <MyMusicDetailStyle.MusicThumbnailImg
-                      onClick={() => window.open(item.videoUrl)}
-                      src={item.thumbnailUrl}
-                    ></MyMusicDetailStyle.MusicThumbnailImg>
-                    <MyMusicDetailStyle.VideoTitle>
-                      {item.videoTitle}
-                    </MyMusicDetailStyle.VideoTitle>
-                    <MyMusicDetailStyle.ChannelNameData>
-                      {item.channelTitle}
-                    </MyMusicDetailStyle.ChannelNameData>
-                    <MyMusicDetailStyle.ApplyDate>
-                      {createdDate}
-                    </MyMusicDetailStyle.ApplyDate>
-                    <BsTrash
-                      style={{ cursor: "pointer" }}
-                      onClick={() => deleteMyWakeupSong(item.id)}
-                    />
-                  </MyMusicDetailStyle.MusicInfoContainer>
-                </Suspense>
-              </ErrorBoundary>
-            );
-          })
-        ) : (
-          <ErrorBoundary fallback={<div>error ...</div>}>
-            <Suspense fallback={<h1>Loading...</h1>}>
-              <MyMusicDetailStyle.MusicNull>
-                신청한 기상송이 없쓰껄
-              </MyMusicDetailStyle.MusicNull>
-            </Suspense>
-          </ErrorBoundary>
-        )}
-      </MyMusicDetailStyle.MyMusicListWrap>
+      <ErrorBoundary fallback={<div>error ...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <MyMusicList />
+        </Suspense>
+      </ErrorBoundary>
     </MyMusicDetailStyle.MyMusicDetailWrapContainer>
   );
 };
--- src/queries/myAllWakeupSong/myAllWakeupSong.query.ts
+++ src/queries/myAllWakeupSong/myAllWakeupSong.query.ts
@@ -1,7 +1,23 @@
-import { useQuery } from "react-query";
+import { AxiosError } from "axios";
+import { useQuery, UseQueryOptions } from "react-query";
 import wakeupSongRepository from "repository/wakeupSong/wakeupSong.repository";
+import { WakeupSongMusicLists } from "types/wakeupSongMy/wakeupSongMy.type.";
+// import { TodoResponse } from "../../types.todo";
 
-export const useGetMyAllWakeupSong = () =>
-  useQuery("myAllWakeupSong/useGetMyAllWakeupSong", () =>
-    wakeupSongRepository.getWakeupSongMy()
+export const useGetMyAllWakeupSong = (
+  options?: UseQueryOptions<
+    WakeupSongMusicLists,
+    AxiosError,
+    WakeupSongMusicLists,
+    "myAllWakeupSong/useGetMyAllWakeupSong"
+    // myAllWakeupSong.useGetMyAllWakeupSong
+    // suspense : true,
+  >
+) =>
+  useQuery(
+    "myAllWakeupSong/useGetMyAllWakeupSong",
+    () => wakeupSongRepository.getWakeupSongMy(),
+    {
+      ...options,
+    }
   );
