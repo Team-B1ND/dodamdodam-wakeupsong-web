71ee76f01929db33dd9f4968d34037c584a46b97
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-03-08 23:56:49 +0900

    refactor: 사용하지 않는 DibStorage 삭제

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../Header";
 import Main from "../Main";
 import Nav from "../Nav";
-import DibsStorage from "components/DibsStorage";
 import MyMusicStore from "components/MyMusicDetail";
 import TodayMusicDetail from "components/TodayMusicDetail";
 import PendingMusicDetail from "components/PendingMusicDetail";
@@ -13,7 +12,6 @@ const Router = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/dibsstorage" element={<DibsStorage />} />
         <Route path="/mymusicdetail" element={<MyMusicStore />} />
         <Route path="/todaymusicdetail" element={<TodayMusicDetail />} />
         <Route path="/pendingmusicdetail" element={<PendingMusicDetail />} />
