6a2cbfb9f28ae68c0e8f4ab61bafcd66dce46354
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-03-14 09:37:07 +0900

    chore: basename 추가

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -8,7 +8,7 @@ import PendingMusicDetail from "components/PendingMusicDetail";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/wakesong">
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
