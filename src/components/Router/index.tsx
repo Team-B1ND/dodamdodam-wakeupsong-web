5ce3906fe023f7851ff350bf74e530d2a707ee3f
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2025-03-03 13:56:52 +0900

    remove :: 파일 삭제 #32

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,22 +1,12 @@
+import WakeupSongPage from "pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../common/Header";
-import Main from "../Main";
-import Nav from "../common/Nav";
-import MyMusic from "components/MyMusic";
-import ApproveMusic from "components/ApproveMusic";
-import PendingMusic from "components/PendingMusic";
 
 const Router = () => {
   return (
     <BrowserRouter basename="/wakesong">
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/mymusic" element={<MyMusic />} />
-        <Route path="/todaymusic" element={<ApproveMusic />} />
-        <Route path="/pendingmusic" element={<PendingMusic />} />
+        <Route path="/" element={<WakeupSongPage />} />
       </Routes>
-      <Nav />
     </BrowserRouter>
   );
 };
